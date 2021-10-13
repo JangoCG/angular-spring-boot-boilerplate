d6be401c184e04ef27eb585575b4cb220b0a3ecf
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2021-10-13 21:21:54 +0200

    add spring backend

---
+++ backend/src/test/java/com/jango/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jango.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
